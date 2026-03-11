e0cd7a980e45eff74bee792c3fd96cbfd8f03267
Ukasyah <hi.ukasyah@gmail.com>
2026-03-11 20:23:03 +0700

    feat: add clone macro into env

--- src/env.rs
+++ src/env.rs
@@ -7,7 +7,7 @@ pub fn load_env() {
     env_logger::Builder::from_env(LogEnv::default().default_filter_or("info")).init();
 }
 
-#[derive(Debug, Envconfig)]
+#[derive(Clone, Debug, Envconfig)]
 pub struct BaseEnv {
     #[envconfig(nested)]
     pub cors: Cors,
@@ -17,19 +17,19 @@ pub struct BaseEnv {
     pub openapi: OpenApi,
 }
 
-#[derive(Debug, Envconfig)]
+#[derive(Clone, Debug, Envconfig)]
 pub struct Cors {
     #[envconfig(from = "CORS_ALLOWED_ORIGINS")]
     pub allowed_origins: Option<String>,
 }
 
-#[derive(Debug, Envconfig)]
+#[derive(Clone, Debug, Envconfig)]
 pub struct Http {
     #[envconfig(from = "HTTP_ADDRESS", default = "127.0.0.1:3000")]
     pub address: String,
 }
 
-#[derive(Debug, Envconfig)]
+#[derive(Clone, Debug, Envconfig)]
 pub struct OpenApi {
     #[envconfig(nested)]
     pub info: OpenApiInfo,
@@ -37,7 +37,7 @@ pub struct OpenApi {
     pub server_urls: String,
 }
 
-#[derive(Debug, Envconfig)]
+#[derive(Clone, Debug, Envconfig)]
 pub struct OpenApiInfo {
     #[envconfig(from = "OPENAPI_INFO_TITLE", default = "")]
     pub title: String,
